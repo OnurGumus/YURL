477daf7536c748e4f7b1eb127bed044c6792b03d
Onur Gumus <onur@outlook.com.tr>
2025-05-11 17:42:18 +0000

    load html

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -4,21 +4,35 @@ open Giraffe
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.AspNetCore.Http
-
+open Fue.Data
+open Fue.Compiler
+open System.IO
 let builder= WebApplication.CreateBuilder()
 
 let rootHandler: HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
+        let contentRootPath =  ctx.GetWebHostEnvironment().ContentRootPath
+        let indexHtml = Path.Combine(contentRootPath, "templates", "index.html")
         task {
-            return! text "Hello World" next ctx
+            let compiledHtml =
+                init
+                |> add "someValue" "myValue"
+                |> fromFile indexHtml
+            return! htmlString compiledHtml next ctx
         }
 
 builder.Services.AddGiraffe() |> ignore
 
-builder.WebHost.ConfigureLogging(fun logging ->
-    logging.ClearProviders().AddConsole().AddDebug() |> ignore
+builder.WebHost.ConfigureLogging(fun ctx logging ->
+    if ctx.HostingEnvironment.IsDevelopment() then
+        logging.ClearProviders().AddConsole().AddDebug() |> ignore
+    else
+        logging.ClearProviders().AddConsole() |> ignore
+).ConfigureAppConfiguration(fun ctx config -> 
+        ctx.HostingEnvironment.ApplicationName <- "tyni.ai" 
 ) |> ignore
+
 let app = builder.Build()
 
-app.UseGiraffe(rootHandler)
+rootHandler |> app.UseGiraffe
 app.Run()
