c900d157f1d8007de965e304a8f0950c9a83823f
Onur Gumus <onur@outlook.com.tr>
2025-05-26 18:47:46 +0000

    fix ip logging

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -132,26 +132,26 @@ builder.WebHost
 let private getClientIp (request: obj) : string =
     let proxyRequest = request :?> IIncomingHttpRequestProxy
     let httpContext = proxyRequest.Request.HttpContext
-    //let logger = httpContext.RequestServices.GetService<ILogger>() |> nonNull
+    let logger = httpContext.RequestServices.GetService<ILogger<obj>>() |> nonNull
     
     let ipAddress =
         match httpContext.Connection.RemoteIpAddress with
         | null -> 
-         //   logger.LogWarning("RemoteIpAddress is null, using 'unknown_ip'")
+            logger.LogWarning("RemoteIpAddress is null, using 'unknown_ip'")
             "unknown_ip"
         | addr ->
             let ip = addr.ToString()
-      //      logger.LogInformation("Extracted IP address: {IpAddress}", ip)
+            logger.LogInformation("Extracted IP address: {IpAddress}", ip)
             ip
     
     // Also log headers that might contain the real IP
-    // let headers = httpContext.Request.Headers
-    // if headers.ContainsKey("X-Forwarded-For") then
-    //     logger.LogInformation("X-Forwarded-For header: {XForwardedFor}", headers.["X-Forwarded-For"].ToString())
-    // if headers.ContainsKey("X-Real-IP") then
-    //     logger.LogInformation("X-Real-IP header: {XRealIP}", headers.["X-Real-IP"].ToString())
-    // if headers.ContainsKey("CF-Connecting-IP") then
-    //     logger.LogInformation("CF-Connecting-IP header: {CFConnectingIP}", headers.["CF-Connecting-IP"].ToString())
+    let headers = httpContext.Request.Headers
+    if headers.ContainsKey("X-Forwarded-For") then
+        logger.LogInformation("X-Forwarded-For header: {XForwardedFor}", headers.["X-Forwarded-For"].ToString())
+    if headers.ContainsKey("X-Real-IP") then
+        logger.LogInformation("X-Real-IP header: {XRealIP}", headers.["X-Real-IP"].ToString())
+    if headers.ContainsKey("CF-Connecting-IP") then
+        logger.LogInformation("CF-Connecting-IP header: {CFConnectingIP}", headers.["CF-Connecting-IP"].ToString())
     
     ipAddress
 
